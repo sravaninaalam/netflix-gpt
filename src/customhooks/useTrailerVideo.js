603fea92a685f2572d3742be7eca5d535b5f5816
sravaninaalam <sravaninaalam2000@gmail.com>
2023-09-13 22:03:59 +0530

    More trailer filters added

--- src/customhooks/useTrailerVideo.js
+++ src/customhooks/useTrailerVideo.js
@@ -12,7 +12,11 @@ const useTrailerVideo = (movieId) => {
         const data=await fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos',API_OPTIONS)
         const json=await data.json()
        
-        const trailer=json?.results.filter(video=>video.type==="Trailer")
+        const trailer=json?.results.filter((video)=>video.type==="Trailer" || 
+                                            video.type === "Official Trailer" ||
+                                            video.type === "Teaser" ||
+                                            video.type === "Featurette" ||
+                                            video.type.toLowerCase().includes("trailer"))
           const result=trailer.length ?trailer[0] :json?.results[0]
           // const videokey=result[0]?.key
           dispatch(addTrailer(result))
