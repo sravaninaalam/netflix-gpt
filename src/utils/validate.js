ba5ffcc7aa385d4404f9c6efdba6931a3c9c0a20
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-02-05 14:47:40 +0530

    necessary configs

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,6 @@
 export const checkValidData=(email,password)=>{
 
-    const isMailValid=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
+    const isMailValid=/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
     const isPasswordValid=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$/.test(password)
    
     if(!isMailValid) return "Mail is not valid"
